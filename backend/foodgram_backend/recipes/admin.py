624e643de38dcd83ae653bebb3025da5fd08e592
Александр <tarasov.slavas2002@gmail.com>
2022-08-15 19:39:46 +0000

    added filters and changed displayed fields

--- backend/foodgram_backend/recipes/admin.py
+++ backend/foodgram_backend/recipes/admin.py
@@ -3,11 +3,23 @@ from django.contrib import admin
 from .models import Ingridient, Recipe, RecipeIngridients, Tag
 
 
+
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'colored_name')
+    list_display = ('name', 'slug', 'colored_name',)
 
 
-admin.site.register(Tag, TagAdmin)
-admin.site.register(Recipe)
-admin.site.register(Ingridient)
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author',)
+    list_filter = ('author', 'name', 'tag',)
+    readonly_fields = ('wish_list_count',)
+
+
+@admin.register(Ingridient)
+class IngridientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit',)
+    list_filter = ('name',)
+
+
 admin.site.register(RecipeIngridients)
