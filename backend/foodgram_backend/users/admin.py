b8a7e7bcbdcf4b7036f8609174c99446f9531bc1
Александр <tarasov.slavas2002@gmail.com>
2022-08-15 19:40:24 +0000

    added filter

--- backend/foodgram_backend/users/admin.py
+++ backend/foodgram_backend/users/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from .models import Follow, User
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_filter = ('email', 'username')
+
+
 admin.site.register(Follow)
