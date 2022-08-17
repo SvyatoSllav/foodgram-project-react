e38f408fc274bc4e78df408167cd7adc7be1d272
Александр <tarasov.slavas2002@gmail.com>
2022-08-17 19:44:17 +0000

    added viewsets for tags and ingridients

---
+++ backend/foodgram_backend/api/recipe/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import get_list_or_404, get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+
+from recipes.models import Ingridient, Tag
+
+from .serializers import IngridientSerializer, TagSerializer
+
+
+class TagViewSet(ViewSet):
+
+    def list(self, request):
+        queryset = get_list_or_404(Tag)
+        serializer = TagSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        tag = get_object_or_404(Tag, pk=pk)
+        serializer = TagSerializer(tag, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class IngridientViewSet(ViewSet):
+
+    def list(self, request):
+        queryset = get_list_or_404(Ingridient)
+        serializer = IngridientSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        tag = get_object_or_404(Ingridient, pk=pk)
+        serializer = IngridientSerializer(tag, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
