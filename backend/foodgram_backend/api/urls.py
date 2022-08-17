e062bd76991c552b66abf117b51855a89ba8f688
Александр <tarasov.slavas2002@gmail.com>
2022-08-17 19:42:50 +0000

    added routers for Tags and Ingridients

--- backend/foodgram_backend/api/urls.py
+++ backend/foodgram_backend/api/urls.py
@@ -4,11 +4,18 @@ from rest_framework.routers import SimpleRouter
 
 from .users_auth.views import CustomAuthToken, DeleteToken, UserViewSet
 
+from .recipe.views import TagViewSet, IngridientViewSet
+
+
 router = SimpleRouter()
 router.register('users', UserViewSet, basename='users')
+router.register('tags', TagViewSet, basename='tags')
+router.register('ingridient', IngridientViewSet, basename='ingridient')
 
 urlpatterns = [
     path('', include(router.urls)),
+
+    # Auth urls
     path('auth/token/login/', CustomAuthToken.as_view()),
     path('auth/token/logout/', DeleteToken.as_view()),
 ]
