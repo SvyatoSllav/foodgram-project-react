35b71888b3e332a5eab970aa1888fa4998a8ba4f
Александр <tarasov.slavas2002@gmail.com>
2022-08-19 12:25:13 +0000

    added viewsets for recipes

--- backend/foodgram_backend/api/urls.py
+++ backend/foodgram_backend/api/urls.py
@@ -2,14 +2,22 @@ from django.urls import include, path
 
 from rest_framework.routers import SimpleRouter
 
-from .recipe.views import IngridientViewSet, TagViewSet
-from .users_auth.views import CustomAuthToken, DeleteToken, UserViewSet
-
+from .recipe.views import (
+    IngredientViewSet,
+    RecipeViewSet,
+    TagViewSet
+)
+from .users_auth.views import (
+    CustomAuthToken,
+    DeleteToken,
+    UserViewSet,
+)
 
 router = SimpleRouter()
 router.register('users', UserViewSet, basename='users')
 router.register('tags', TagViewSet, basename='tags')
-router.register('ingridient', IngridientViewSet, basename='ingridient')
+router.register('ingredient', IngredientViewSet, basename='ingredient')
+router.register('recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('', include(router.urls)),
