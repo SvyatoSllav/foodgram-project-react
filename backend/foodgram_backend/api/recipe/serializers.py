06550327ab979383da2672ac08fa847713f085b7
Александр <tarasov.slavas2002@gmail.com>
2022-08-19 12:27:04 +0000

    added serializer for recipes

--- backend/foodgram_backend/api/recipe/serializers.py
+++ backend/foodgram_backend/api/recipe/serializers.py
@@ -1,6 +1,10 @@
-from recipes.models import Ingridient, Tag
+from recipes.models import Ingredient, Recipe, Tag, RecipeIngredients
 
 from rest_framework import serializers
+
+from drf_extra_fields.fields import Base64ImageField
+
+from api.users_auth.serializers import SafeUserSerializer
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -15,12 +19,80 @@ class TagSerializer(serializers.ModelSerializer):
         )
 
 
-class IngridientSerializer(serializers.ModelSerializer):
+class IngredientSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Ingridient
+        model = Ingredient
         fields = (
             'id',
             'name',
             'measurement_unit',
         )
+
+
+class RecipeingredientsSerializer(serializers.ModelSerializer):
+    ingredient = IngredientSerializer(read_only=False, many=False)
+
+    class Meta:
+        model = RecipeIngredients
+        fields = (
+            'ingredient',
+        )
+
+    def to_representation(self, instance):
+        representation = super(RecipeingredientsSerializer, self).to_representation(instance)
+        representation['ingredient']['amount'] = instance.weight
+        return representation
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(
+        source='tag',
+        many=True
+    )
+    author = SafeUserSerializer(
+        read_only=True,
+        many=False
+    )
+    ingredients = RecipeingredientsSerializer(many=True,)
+    is_favorited = serializers.SerializerMethodField()
+    is_in_shopping_cart = serializers.SerializerMethodField()
+    image = Base64ImageField()
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id',
+            'tags',
+            'author',
+            'ingredients',
+            'is_favorited',
+            'is_in_shopping_cart',
+            'name',
+            'image',
+            'text',
+            'cooking_time'
+        )
+
+    def get_is_favorited(self, obj):
+        return bool(
+            self.context.get('request').query_params.get('is_favorited')
+            )
+
+    def get_is_in_shopping_cart(self, obj):
+        return bool(
+            self.context.get('request').query_params.get('is_in_shopping_cart')
+            )
+
+    def to_representation(self, instance):
+        representation = super(RecipeSerializer, self).to_representation(instance)
+        for i in range(len(representation['ingredients'])):
+            representation['ingredients'][i] = representation['ingredients'][i]['ingredient']
+        return representation
+
+    def create(self, validated_data):
+        ingredients = validated_data.pop('ingredients')
+        print(ingredients)
+        recipe = super().create(validated_data)
+
+        return recipe
