0adfb49033592670c28ba0f5930adc85813b3944
Александр <tarasov.slavas2002@gmail.com>
2022-08-19 12:25:21 +0000

    added recipe permission

---
+++ backend/foodgram_backend/api/recipe/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework import permissions
+
+
+class RecipePermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if view.action in ['list', 'retrieve']:
+            return True
+        elif view.action == 'create':
+            return request.user.is_authenticated
+        if view.action in ['partial_update', 'destroy']:
+            return True
+        else:
+            return False
+
+    def has_object_permission(self, request, view, obj):
+        if view.action in ['partial_update', 'destroy']:
+            return obj.author == request.User
+        return True
