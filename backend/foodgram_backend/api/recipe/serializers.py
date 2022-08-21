d5c8b30060fdf0df31a29f45ecbf7633aa90142b
Александр <tarasov.slavas2002@gmail.com>
2022-08-21 19:50:46 +0000

    create and update functionality for recipes

--- backend/foodgram_backend/api/recipe/serializers.py
+++ backend/foodgram_backend/api/recipe/serializers.py
@@ -1,10 +1,12 @@
-from recipes.models import Ingredient, Recipe, Tag, RecipeIngredients
-
-from rest_framework import serializers
+from api.users_auth.serializers import SafeUserSerializer
 
 from drf_extra_fields.fields import Base64ImageField
 
-from api.users_auth.serializers import SafeUserSerializer
+from recipes.models import Ingredient, Recipe, RecipeIngredients, Tag
+
+from rest_framework import serializers
+
+from users.models import Follow
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -40,7 +42,9 @@ class RecipeingredientsSerializer(serializers.ModelSerializer):
         )
 
     def to_representation(self, instance):
-        representation = super(RecipeingredientsSerializer, self).to_representation(instance)
+        representation = super(
+            RecipeingredientsSerializer, self).to_representation(instance)
+
         representation['ingredient']['amount'] = instance.weight
         return representation
 
@@ -85,18 +89,138 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
 
     def to_representation(self, instance):
-        representation = super(RecipeSerializer, self).to_representation(instance)
+        representation = super(
+            RecipeSerializer, self).to_representation(instance)
+
         # Converts {ingredients: [ingredient:{some_values}]}
         # to {ingredients: [{some_values}]}
         for ingredient_index in range(len(representation['ingredients'])):
             ingredient = representation.get('ingredients')[ingredient_index]
             ingredient_value = ingredient.get('ingredient')
-            representation['ingredients'][ingredient] = ingredient_value
+            representation['ingredients'][ingredient_index] = ingredient_value
         return representation
+
+
+class CreateUpdateRecipeSerializer(serializers.ModelSerializer):
+    ingredients = serializers.ListField(
+        child=serializers.DictField(
+            child=serializers.IntegerField(min_value=0))
+    )
+    tags = serializers.ListField(
+        child=serializers.IntegerField(min_value=0)
+    )
+    image = Base64ImageField()
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'ingredients',
+            'tags',
+            'image',
+            'name',
+            'text',
+            'cooking_time',
+        )
 
     def create(self, validated_data):
         ingredients = validated_data.pop('ingredients')
-        print(ingredients)
-        recipe = super().create(validated_data)
+        tag_ids = validated_data.pop('tags')
+
+        recipe = Recipe.objects.create(
+            **validated_data,
+            author=self.context['request'].user,
+        )
+
+        for value in ingredients:
+            recipe_ingredient = RecipeIngredients.objects.create(
+                ingredient=Ingredient.objects.get(id=value.get('id')),
+                weight=value.get('amount')
+            )
+            recipe.ingredients.add(recipe_ingredient)
+
+        for id in tag_ids:
+            recipe.tag.add(Tag.objects.get(id=id))
 
         return recipe
+
+    def to_representation(self, instance):
+        current_user = self.context['request'].user
+
+        tags = instance.tag.all()
+        tags_fields_to_representation = [
+            {
+                'id': tag.id,
+                'name': tag.name,
+                'color': tag.color,
+                'slug': tag.slug
+            } for tag in tags
+        ]
+
+        author = instance.author
+        author_to_representation = {
+            'email': author.email,
+            'id': author.id,
+            'username': author.username,
+            'first_name': author.first_name,
+            'last_name': author.last_name,
+            'is_subscribed': Follow.objects.filter(
+                                                followers=current_user,
+                                                following=author).exists()
+        }
+
+        ingredients = instance.ingredients.all()
+        ingredients_fields_to_representation = [
+            {
+                'id': ingredient.ingredient.id,
+                'name': ingredient.ingredient.name,
+                'measurement_unit': ingredient.ingredient.measurement_unit,
+                'amount': ingredient.weight
+            } for ingredient in ingredients
+        ]
+
+        is_favorited = current_user.wish_list.through.objects.filter(
+            recipe__id=instance.id
+            ).exists()
+
+        is_in_shopping_cart = current_user.shop_list.through.objects.filter(
+            recipe__id=instance.id
+            ).exists()
+
+        representation = {
+            'id': instance.id,
+            'tags': tags_fields_to_representation,
+            'author': author_to_representation,
+            'ingredients': ingredients_fields_to_representation,
+            'is_favorited': is_favorited,
+            'is_in_shopping_cart': is_in_shopping_cart,
+            'name': instance.name,
+            'image': 'http://foodgram.example.org/' + instance.image.url,
+            'test': instance.text,
+            'cooking_time': instance.cooking_time
+        }
+        return representation
+
+    def update(self, instance, validated_data):
+        ingredients = validated_data.pop('ingredients')
+        tag_ids = validated_data.pop('tags')
+
+        instance.name = validated_data.pop('name')
+        instance.text = validated_data.pop('text')
+        instance.image = validated_data.pop('image')
+        instance.cooking_time = validated_data.pop('cooking_time')
+
+        instance.ingredients.all().delete()
+        for value in ingredients:
+            recipe_ingredient, created = RecipeIngredients.objects.get_or_create(
+                ingredient=Ingredient.objects.get(id=value.get('id')),
+                weight=value.get('amount')
+            )
+            instance.ingredients.add(recipe_ingredient)
+
+        instance.tag.clear()
+        for id in tag_ids:
+            instance.tag.add(Tag.objects.get(id=id))
+
+        instance.save()
+
+        return instance
