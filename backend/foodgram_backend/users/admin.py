897e8d3b760e92f618b9c398386277a3a8b4af58
Александр <tarasov.slavas2002@gmail.com>
2022-09-06 20:35:31 +0000

    fixes format

--- backend/foodgram_backend/users/admin.py
+++ backend/foodgram_backend/users/admin.py
@@ -5,7 +5,7 @@ from .models import Follow, User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_filter = ('email', 'username')
+    list_filter = ("email", "username")
 
 
 admin.site.register(Follow)
