7bf23c174b575eb0f84ca563778b5007e1551c0c
Александр <tarasov.slavas2002@gmail.com>
2022-09-07 19:50:33 +0000

    changed to tuple

--- backend/foodgram_backend/api/recipe/permissions.py
+++ backend/foodgram_backend/api/recipe/permissions.py
@@ -3,16 +3,16 @@ from rest_framework import permissions
 
 class RecipePermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        if view.action in ["list", "retrieve"]:
+        if view.action in ("list", "retrieve"):
             return True
         elif view.action == "create":
             return request.user.is_authenticated
-        elif view.action in ["partial_update", "destroy"]:
+        elif view.action in ("partial_update", "destroy"):
             return True
         else:
             return False
 
     def has_object_permission(self, request, view, obj):
-        if view.action in ["partial_update", "destroy"]:
+        if view.action in ("partial_update", "destroy"):
             return obj.author == request.user
         return True
